e80ce747558933a106c0647ac6739ea057708416
Fe <64690761+chysis@users.noreply.github.com>
2024-04-26 00:54:25 +0900

    [Gold IV] Title: 최대 정사각형, Time: 2640 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Gold/4095. 최대 정사각형/최대 정사각형.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <memory.h>
+using namespace std;
+
+int arr[1001][1001];
+int dp[1001][1001];
+
+int main()
+{
+    int N, M;
+    while(true){
+        cin>>N>>M;
+        if(N==0 && M==0) break;
+        
+        memset(dp, 0, sizeof(dp));
+        
+        int ans=0;
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=M; j++){
+                cin>>arr[i][j];
+                if(arr[i][j]==1){
+                    dp[i][j]=min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))+1;
+                    ans=max(ans, dp[i][j]);
+                }
+            }
+        }
+        
+        cout<<ans<<"\n";
+    }
+}
