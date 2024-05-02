206414d707b7b8a6d7653b90ae85e74ff471cbb6
Fe <64690761+chysis@users.noreply.github.com>
2024-05-02 17:56:57 +0900

    [Bronze I] Title: A/B - 2, Time: 0 ms, Memory: 2020 KB, Score: 18 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cout<<fixed;
+    cout.precision(1000);
+    
+    double A, B;
+    cin>>A>>B;
+    cout<<A/B;
+}
