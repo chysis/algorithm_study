c2147a205e53ea0b1ecd52988ed70b05cffe3436
Fe <64690761+chysis@users.noreply.github.com>
2024-05-02 22:35:22 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N;
+    cin>>N;
+    
+    cout<<2000<<"\n";
+    for(int i=0; i<1000; i++){
+        cout<<1<<" ";
+    }
+    for(int i=0; i<1000; i++){
+        cout<<1000<<" ";
+    }
+}
