a532119663c34ed252b3e034606354980e381fbf
Fe <64690761+chysis@users.noreply.github.com>
2024-06-03 16:49:37 +0900

    [Gold IV] Title: 빙산, Time: 296 ms, Memory: 3000 KB -BaekjoonHub

---
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int N,M;
+int ice[301][301];
+int near[301][301];
+bool visited[301][301];
+int dx[4]={-1, 1, 0, 0};
+int dy[4]={0, 0, -1, 1};
+int cnt=0;
+
+bool noIce(){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            if(ice[i][j]!=0)
+                return false;
+        }
+    }
+    return true;
+}
+
+void countZero(){
+    memset(near, 0, sizeof(near));
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            int temp=0;
+            for(int k=0; k<4; k++){
+                int ni=i+dx[k];
+                int nj=j+dy[k];
+                if(ni>=0 && ni<N && nj>=0 && nj<M && ice[ni][nj]==0){
+                    temp++;
+                }
+            }
+            near[i][j]=temp;
+        }
+    }
+}
+
+void iceMelt(){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            ice[i][j]-=near[i][j];
+            if(ice[i][j]<0) ice[i][j]=0;
+        }
+    }
+}
+
+void dfs(int x, int y){
+    visited[x][y]=true;
+    for(int i=0; i<4; i++){
+        int nx=x+dx[i];
+        int ny=y+dy[i];
+        if(nx>=0 && nx<N && ny>=0 && ny<M && !visited[nx][ny] && ice[nx][ny]>0){
+            dfs(nx, ny);
+        }
+    }
+}
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    cin>>N>>M;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cin>>ice[i][j];
+        }
+    }
+    
+    int t=0;
+    while(true){
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(!visited[i][j] && ice[i][j]>0){
+                    dfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+        if(noIce()) {t=0; break;}
+        if(cnt>=2) break;
+        t++;
+        countZero();
+        iceMelt();
+        cnt=0;
+        memset(visited, false, sizeof(visited));
+    }
+    cout<<t;
+}
