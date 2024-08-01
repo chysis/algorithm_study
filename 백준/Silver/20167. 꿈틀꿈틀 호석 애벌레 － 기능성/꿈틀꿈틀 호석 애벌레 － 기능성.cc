a937df8cf26ba51550c5164544873693eb99bd81
Fe <64690761+chysis@users.noreply.github.com>
2024-08-02 01:25:57 +0900

    [Silver I] Title: 꿈틀꿈틀 호석 애벌레 - 기능성, Time: 0 ms, Memory: 2020 KB, Score: 31 point -BaekjoonHub

---
+++ 백준/Silver/20167. 꿈틀꿈틀 호석 애벌레 － 기능성/꿈틀꿈틀 호석 애벌레 － 기능성.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, K, ans=0;
+int arr[21], dp[21];
+
+void solve(int cnt, int sati, int energy){
+    if(cnt==N) {
+        if(sati>=K) energy+=(sati-K);
+        ans=max(ans, energy);
+        return;
+    }
+    
+    // 안 먹으면?
+    solve(cnt+1, sati, energy);
+    
+    // 먹으면?
+    if(sati+arr[cnt]>=K) solve(cnt+1, 0, energy+sati+arr[cnt]-K);
+    else solve(cnt+1, sati+arr[cnt], energy);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin>>N>>K;
+    for(int i=0; i<N; i++){
+        cin>>arr[i];
+    }
+    
+    solve(0, 0, 0);
+    cout<<ans;
+}
