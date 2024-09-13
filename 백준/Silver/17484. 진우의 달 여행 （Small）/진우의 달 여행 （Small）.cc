f03e791712c9bf35392628f9389efb589979a528
Fe <64690761+chysis@users.noreply.github.com>
2024-09-14 01:21:00 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+
+#define INF 987654321
+using namespace std;
+
+int N, M;
+int arr[7][7], dp[7][7][3];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin>>N>>M;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            for(int k=0; k<3; k++){
+                dp[i][j][k]=INF;
+            }
+        }
+    }
+    
+    for(int i=0; i<M; i++){
+        if(i==0){
+            dp[1][i][1]=arr[0][i]+arr[1][i];
+            dp[1][i][2]=arr[0][i+1]+arr[1][i];
+        } else if(i==M-1){
+            dp[1][i][0]=arr[0][i-1]+arr[1][i];
+            dp[1][i][1]=arr[0][i]+arr[1][i];
+        } else{
+            dp[1][i][0]=arr[0][i-1]+arr[1][i];
+            dp[1][i][1]=arr[0][i]+arr[1][i];
+            dp[1][i][2]=arr[0][i+1]+arr[1][i];
+        }
+    }
+    
+    for(int i=2; i<N; i++){
+        for(int j=0; j<M; j++){
+            if(j==0){
+                dp[i][j][1]=dp[i-1][j][2]+arr[i][j];
+                dp[i][j][2]=min(dp[i-1][j+1][0], dp[i-1][j+1][1])+arr[i][j];
+            } else if(j==M-1){
+                dp[i][j][0]=min(dp[i-1][j-1][1], dp[i-1][j-1][2])+arr[i][j];
+                dp[i][j][1]=dp[i-1][j][0]+arr[i][j];
+            } else {
+                dp[i][j][0]=min(dp[i-1][j-1][1], dp[i-1][j-1][2])+arr[i][j];
+                dp[i][j][1]=min(dp[i-1][j][0], dp[i-1][j][2])+arr[i][j];
+                dp[i][j][2]=min(dp[i-1][j+1][0], dp[i-1][j+1][1])+arr[i][j];
+            }
+        }
+    }
+    
+    int ans=INF;
+    for(int i=0; i<M; i++){
+        for(int j=0; j<3; j++){
+            ans=min(ans, dp[N-1][i][j]);
+        }
+    }
+    
+    cout<<ans;
+}
