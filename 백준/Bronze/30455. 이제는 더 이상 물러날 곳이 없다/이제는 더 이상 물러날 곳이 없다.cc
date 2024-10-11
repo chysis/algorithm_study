5f41b65d346b95c3dee3848a548bed4caad150d7
Fe <64690761+chysis@users.noreply.github.com>
2024-10-12 00:23:55 +0900

    [Bronze I] Title: 이제는 더 이상 물러날 곳이 없다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30455. 이제는 더 이상 물러날 곳이 없다/이제는 더 이상 물러날 곳이 없다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N;
+    cin>>N;
+    if(N%2==0) cout<<"Duck";
+    else cout<<"Goose";
+}
