97c3a703d9c85d752c0fe94dae50556db1e36de0
Fe <64690761+chysis@users.noreply.github.com>
2024-10-23 21:54:34 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+long long dp[68];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    dp[0]=dp[1]=1;
+    dp[2]=2;
+    dp[3]=4;
+    for(int i=4; i<=67; i++){
+        dp[i]=dp[i-1]+dp[i-2]+dp[i-3]+dp[i-4];
+    }
+    
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        cout<<dp[N]<<"\n";
+    }
+}
