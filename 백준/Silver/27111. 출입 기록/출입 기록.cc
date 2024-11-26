aa96e4d8a62d5be4f61b1001df498d9ff4b69cae
Fe <64690761+chysis@users.noreply.github.com>
2024-11-26 15:58:26 +0900

    [Silver V] Title: 출입 기록, Time: 32 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/27111. 출입 기록/출입 기록.cc
+++ 백준/Silver/27111. 출입 기록/출입 기록.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+
+#define INF 987654321
 using namespace std;
 
 bool visited[200001];
@@ -9,12 +11,14 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    int N, ans=0, mx=-1;
+    int N, ans=0, mn=INF, mx=-1;
     cin>>N;
     for(int i=0; i<N; i++){
         int A, B;
         cin>>A>>B;
         mx=max(mx, A);
+        mn=min(mn, A);
+        
         if(B==1){
             if(visited[A]) ans++;
             else visited[A]=true;
@@ -24,7 +28,7 @@ int main()
         }
     }
     
-    for(int i=1; i<=mx; i++){
+    for(int i=mn; i<=mx; i++){
         if(visited[i]) ans++;
     }
     
