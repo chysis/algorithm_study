f38a9625a8752328ab88617a698b01c80598cf2c
Fe <64690761+chysis@users.noreply.github.com>
2024-12-02 16:38:12 +0900

    [Gold V] Title: 행복 유치원, Time: 60 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[300001], diff[300001];
+
+bool comp(int a, int b){
+    return a>b;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N, K;
+    cin>>N>>K;
+    for(int i=0; i<N; i++){
+        cin>>arr[i];
+    }
+    
+    for(int i=0; i<N-1; i++){
+        diff[i]=arr[i+1]-arr[i];
+    }
+    
+    sort(diff, diff+N-1, comp);
+    
+    int ans=arr[N-1]-arr[0];
+    for(int i=0; i<K-1; i++){
+        ans-=diff[i];
+    }
+    
+    cout<<ans;
+}
