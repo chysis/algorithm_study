48e40e942bddd0d220bc64e4e5f5b51c362eac56
Fe <64690761+chysis@users.noreply.github.com>
2024-12-21 17:37:11 +0900

    [Gold III] Title: 크게 만들기, Time: 8 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.cc
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-vector<int> v;
+vector<char> v;
 
 int main()
 {
@@ -15,12 +15,11 @@ int main()
     cin>>N>>K>>str;
     
     for(int i=0; i<N; i++){
-        int cur=str[i]-'0';
-        while(!v.empty() && v.back()<cur && K>0){
+        while(!v.empty() && v.back()<str[i] && K>0){
             v.pop_back();
             K--;
         }
-        v.push_back(cur);
+        v.push_back(str[i]);
     }
     
     for(int i=0; i<v.size()-K; i++) cout<<v[i];
