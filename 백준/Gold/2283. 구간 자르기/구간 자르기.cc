ff845b585a6ef78e7c5f52ef6a12f6cff48c7137
Fe <64690761+chysis@users.noreply.github.com>
2024-12-27 16:41:36 +0900

    [Gold II] Title: 구간 자르기, Time: 8 ms, Memory: 9840 KB -BaekjoonHub

---
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+
+#define INF (~0U>>2)
+using namespace std;
+
+pair<int, int> arr[1001];
+int delta[1000010], sum[1000010];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N, K, mn=INF, mx=-1;
+    cin>>N>>K;
+    for(int i=0; i<N; i++){
+        int S, E;
+        cin>>S>>E;
+        mn=min(mn, S);
+        mx=max(mx, E);
+        arr[i]={S, E};
+    }
+    
+    sort(arr, arr+N);
+    
+    for(int i=0; i<N; i++){
+        delta[arr[i].first+1]++;
+        delta[arr[i].second+1]--;
+    }
+    
+    int temp=0;
+    for(int i=mn; i<=mx; i++){
+        temp+=delta[i];
+        delta[i]=temp;
+    }
+    
+    sum[mn]=delta[mn];
+    for(int i=mn+1; i<=mx; i++){
+        sum[i]=sum[i-1]+delta[i];
+    }
+    
+    // for(int i=0; i<=mx; i++){
+    //     cout<<sum[i]<<" ";
+    // }
+    // cout<<"\n";
+    
+    if(sum[mx]<K){
+        cout<<0<<" "<<0;
+        return 0;
+    }
+    
+    int s=0, e=0;
+    pair<int, int> ans={INF, INF};
+    bool flag=false;
+    while(s<=e){
+        int diff=sum[e]-sum[s];
+        // cout<<"cur: "<<s<<" "<<e<<" "<<diff<<"\n";
+        
+        if(diff<K) e++;
+        else if(diff>K) s++;
+        else{
+            cout<<s<<" "<<e;
+            flag=true;
+            break;
+        }
+    }
+    
+    if(!flag) cout<<0<<" "<<0;
+}
