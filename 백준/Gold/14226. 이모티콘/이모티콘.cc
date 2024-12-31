46c6b205fd3f5323c123b785bb9b9e1a4414c7bc
Fe <64690761+chysis@users.noreply.github.com>
2024-12-31 18:01:52 +0900

    [Gold IV] Title: 이모티콘, Time: 4 ms, Memory: 6196 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.cc
+++ 백준/Gold/14226. 이모티콘/이모티콘.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 int S;
-queue<tuple<int, int, int>> q; // 현재 개수, 클립보드에 있는 개수, cnt
+queue<tuple<int, int, int>> q;
 bool visited[2001][2001];
 
 int bfs(){
@@ -14,20 +14,15 @@ int bfs(){
         int cnt=get<2>(q.front());
         q.pop();
         
-        // cout<<"cur: "<<cur<<" clipboard: "<<clipboard<<" cnt: "<<cnt<<"\n";
-        
         if(cur==S) return cnt;
 
-        // 클립보드에 복사
         q.push({cur, cur, cnt+1});
         
-        // 클립보드에서 붙여넣기
         if(!visited[cur+clipboard][clipboard] && cur+clipboard<=S){
             q.push({cur+clipboard, clipboard, cnt+1});
             visited[cur+clipboard][clipboard]=true;
         }
         
-        // 하나 삭제
         if(cur-1>0 && !visited[cur-1][clipboard]){
             q.push({cur-1, clipboard, cnt+1});
             visited[cur-1][clipboard]=true;
