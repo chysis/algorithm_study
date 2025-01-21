3d36643c3f6b1c6983410bd3d2ab4a966f8ecead
Fe <64690761+chysis@users.noreply.github.com>
2025-01-21 18:12:21 +0900

    [Gold II] Title: 공항, Time: 12 ms, Memory: 2900 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int G, P, maxG=-1, g[100001], dock[100001];
+bool used[100001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin>>G>>P;
+    for(int i=1; i<=P; i++){
+        cin>>g[i];
+    }
+
+    for(int i=1; i<=P; i++){
+        bool flag=false;
+        int start=(dock[g[i]]==0 ? g[i] : dock[g[i]]-1);
+        
+        for(int j=start; j>=1; j--){
+            if(!used[j]){
+                used[j]=true;
+                dock[g[i]]=j;
+                flag=true;
+                break;
+            }
+        }
+        
+        if(!flag){
+            cout<<i-1;
+            return 0;
+        }
+    }
+    
+    cout<<P;
+}
