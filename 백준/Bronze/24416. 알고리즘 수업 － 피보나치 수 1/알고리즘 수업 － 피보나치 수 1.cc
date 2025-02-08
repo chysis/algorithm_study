a6c45e11941f17d58d1db794dc243dfb3bdefd7b
Fe <64690761+chysis@users.noreply.github.com>
2025-02-08 21:59:27 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.cc
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.cc
@@ -1,24 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int N, ans1=0, ans2=0, f[41];
-
-int fib(int n){
-    if(n==1 || n==2) {
-        ans1++;
-        return 1;
-    }
-    else return fib(n-1)+fib(n-2);
-}
-
-int fibonacci(int n){
-    f[1]=f[2]=1;
-    for(int i=3; i<=n; i++){
-        f[i]=f[i-1]+f[i-2];
-        ans2++;
-    }
-    return f[n];
-}
+int N, dp[41];
 
 int main()
 {
@@ -26,7 +9,9 @@ int main()
     cin.tie(nullptr);
     
     cin>>N;
-    fib(N);
-    fibonacci(N);
-    cout<<ans1<<" "<<ans2;
+    dp[1]=dp[2]=1;
+    for(int i=3; i<=N; i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+    cout<<dp[N]<<" "<<N-2;
 }
