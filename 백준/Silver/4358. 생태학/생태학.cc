de573dbf2073d9b2499300e805484a6b1a755062
Fe <64690761+chysis@users.noreply.github.com>
2025-03-02 15:31:07 +0900

    [Silver II] Title: 생태학, Time: 288 ms, Memory: 2296 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<string, double> m;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int cnt=0;
+    string str;
+    while(true){
+        getline(cin, str);
+        if(cin.eof()) break;
+        
+        cnt++;
+        if(m.find(str)==m.end()){
+            m[str]=1;
+        } else m[str]++;
+    }
+    
+    cout<<fixed;
+    cout.precision(4);
+    
+    for(auto it=m.begin(); it!=m.end(); it++){
+        cout<<it->first<<" ";
+        cout<<(it->second)*100/cnt<<"\n";
+    }
+}
