07d736918e8b0984a9ea0166aca43765df88a60b
Fe <64690761+chysis@users.noreply.github.com>
2025-03-22 00:12:16 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 60 ms, Memory: 64556 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp[4001][4001]; // dp[i][j]: S의 i번째, T의 j번째까지 비교했을 때 가장 긴 공통 부분 문자열 길이
+int dp[4001][4001];
 string S, T;
 
 int main()
@@ -11,22 +11,15 @@ int main()
     
     cin>>S>>T;
     
-    for(int i=0; i<=S.length(); i++){
-        for(int j=0; j<=T.length(); j++){
-            if(i==0 || j==0) dp[i][j]=0; // set margin
-            else{
-                if(S[i-1]==T[j-1]) dp[i][j]=dp[i-1][j-1]+1;
-                else dp[i][j]=0;
-            }
-        }
-    }
-    
     int ans=-1;
     for(int i=0; i<=S.length(); i++){
         for(int j=0; j<=T.length(); j++){
+            if(i==0 || j==0) dp[i][j]=0;
+            else if(S[i-1]==T[j-1]) dp[i][j]=dp[i-1][j-1]+1;
+            else dp[i][j]=0;
+            
             ans=max(ans, dp[i][j]);
         }
     }
-    
     cout<<ans;
 }
