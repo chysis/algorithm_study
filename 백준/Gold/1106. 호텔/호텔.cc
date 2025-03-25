f576fa581aa2edfb69695814c85574361e29244b
Fe <64690761+chysis@users.noreply.github.com>
2025-03-26 03:27:48 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2112 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define INF (~0U>>2)
+using namespace std;
+
+int C, N, dp[21][1101]; // dp[i][j]: i번째 마을까지 홍보해서 j명 늘리기 위해 필요한 최소 금액
+pair<int, int> arr[21];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin>>C>>N;
+    
+    for(int i=0; i<=N; i++){
+        for(int j=0; j<=C+100; j++){
+            dp[i][j]=INF;
+        }
+    }
+    
+    for(int i=1; i<=N; i++){
+        int A, B;
+        cin>>A>>B;
+        arr[i]={A, B};
+    }
+    
+    // for(int i=0; i<=N; i++){
+    //     dp[i][0]=0;
+    // }
+    
+    // for(int i=0; i<=C; i++){
+    //     dp[0][i]=0;
+    // }
+    
+    for(int i=1; i<=N; i++){
+        dp[i][arr[i].second]=arr[i].first;
+    }
+    
+    // for(int i=0; i<=N; i++){
+    //     for(int j=0; j<=C; j++){
+    //         cout<<dp[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+    // cout<<"\n";
+    
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=C+100; j++){
+            if(j-arr[i].second>0) dp[i][j]=min({dp[i-1][j], arr[i].first+dp[i-1][j-arr[i].second], arr[i].first+dp[i][j-arr[i].second]});
+            else dp[i][j]=min(dp[i][j], dp[i-1][j]);
+        }
+    }
+    
+    // for(int i=0; i<=N; i++){
+    //     for(int j=0; j<=C; j++){
+    //         cout<<dp[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+    // cout<<"\n";
+    
+    int ans=INF;
+    for(int i=C; i<=C+100; i++){
+        ans=min(ans, dp[N][i]);
+    }
+    
+    cout<<ans;
+}
