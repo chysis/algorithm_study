a7e5cde465019d8f94d76c0aee42de1d76b622ee
Fe <64690761+chysis@users.noreply.github.com>
2025-04-02 21:52:56 +0900

    [Gold V] Title: 4와 7, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Gold/2877. 4와 7/4와 7.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int K, newK, dig;
+    cin>>K;
+    
+    for(int i=1; ; i++){
+        if(pow(2, i)-2<K && pow(2, i+1)-2>=K) {
+            dig=i;
+            break;
+        }
+    }
+    
+    newK=(dig==1 ? K-1 : K-pow(2, dig)+1);
+
+    vector<int> v;
+    while(newK>0){
+        v.push_back(newK%2);
+        newK/=2;
+    }
+    
+    string ans(dig, '4');
+    
+    for(int i=0; i<v.size(); i++){
+        ans[dig-i-1]=(v[i]==0?'4':'7');
+    }
+    
+    cout<<ans;
+}
