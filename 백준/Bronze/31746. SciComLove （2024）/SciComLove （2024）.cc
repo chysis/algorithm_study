ad0c8c1b4fdc86862860b3d15cc4617d163d5a8f
Fe <64690761+chysis@users.noreply.github.com>
2025-04-24 02:06:59 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
+    cin>>N;
+    if(N%2==0) cout<<"SciComLove";
+    else cout<<"evoLmoCicS";
+}
