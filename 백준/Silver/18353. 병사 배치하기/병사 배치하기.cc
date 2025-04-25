225f4d7165a10ec3b8e5bb51caaf78324fd6b3ea
Fe <64690761+chysis@users.noreply.github.com>
2025-04-25 16:52:51 +0900

    [Silver II] Title: 병사 배치하기, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, arr[2001], dp[2001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin>>N;
+    for(int i=0; i<N; i++){
+        cin>>arr[i];
+    }
+    
+    for(int i=0; i<N; i++){
+        dp[i]=1;
+        for(int j=0; j<i; j++){
+            if(arr[i]<arr[j]){
+                dp[i]=max(dp[i], dp[j]+1);
+            }
+        }
+    }
+    
+    cout<<N-(*max_element(dp, dp+N));
+}
