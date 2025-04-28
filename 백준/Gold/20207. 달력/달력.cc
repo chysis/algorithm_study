6afe9ba25a2e30ca63981f0db8509cd868e42ab3
Fe <64690761+chysis@users.noreply.github.com>
2025-04-29 01:20:51 +0900

    [Gold V] Title: 달력, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/20207. 달력/달력.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define INF (~0U>>2)
+using namespace std;
+
+int N, S, E, minVal=INF, maxVal=0, h[367];
+pair<int, int> arr[1001];
+
+bool comp(pair<int, int>& a, pair<int, int>& b){
+    if(a.first!=b.first) return a.first<b.first;
+    else return a.second>b.second;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin>>N;
+    for(int i=0; i<N; i++){
+        cin>>S>>E;
+        minVal=min({minVal, S, E});
+        maxVal=max({maxVal, S, E});
+        arr[i]={S, E};
+    }
+    
+    sort(arr, arr+N, comp);
+    
+    for(int i=0; i<N; i++){
+        for(int j=arr[i].first; j<=arr[i].second; j++){
+            h[j]++;
+        }
+    }
+    
+    int ans=0, tempLen=0, tempVal=0;
+    for(int i=minVal; i<=maxVal+1; i++){
+        if(h[i]!=0){
+            tempLen++;
+            tempVal=max(tempVal, h[i]);
+        } else{
+            ans+=(tempLen*tempVal);
+            tempLen=tempVal=0;
+        }
+    }
+    
+    cout<<ans;
+}
