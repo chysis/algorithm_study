3a6c9eabb156673d4ca8f31c200438f9d429532f
Fe <64690761+chysis@users.noreply.github.com>
2025-05-21 02:11:52 +0900

    [Silver IV] Title: 상근이의 여행, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -2,33 +2,6 @@
 using namespace std;
 
 int T, N, M, A, B;
-vector<int> adj[1001];
-bool visited[1001];
-queue<int> q;
-
-void init(){
-    q={};
-    memset(visited, false, sizeof(visited));
-    for(int i=1; i<=N; i++) adj[i].clear();
-}
-
-int solve(){
-    q.push(1);
-    visited[1]=true;
-    int ans=0;
-    while(!q.empty()){
-        int cur=q.front();
-        q.pop();
-        
-        for(auto next: adj[cur]){
-            if(visited[next]) continue;
-            visited[next]=true;
-            ans++;
-            q.push(next);
-        }
-    }
-    return ans;
-}
 
 int main()
 {
@@ -38,13 +11,7 @@ int main()
     cin>>T;
     while(T--){
         cin>>N>>M;
-        for(int i=0; i<M; i++){
-            cin>>A>>B;
-            adj[A].push_back(B);
-            adj[B].push_back(A);
-        }
-
-        cout<<solve()<<"\n";
-        init();
+        for(int i=0; i<M; i++) cin>>A>>B;
+        cout<<N-1<<"\n";
     }
 }
