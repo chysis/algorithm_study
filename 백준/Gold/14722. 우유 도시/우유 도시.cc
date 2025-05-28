baa0ff247f1511f1456a8a442d51d1c1a1938d34
Fe <64690761+chysis@users.noreply.github.com>
2025-05-29 00:43:18 +0900

    [Gold IV] Title: 우유 도시, Time: 108 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Gold/14722. 우유 도시/우유 도시.cc
+++ 백준/Gold/14722. 우유 도시/우유 도시.cc
@@ -6,17 +6,16 @@ int N, arr[1001][1001], dp[1001][1001][3];
 int solve(int x, int y, int now){
     if(x==N || y==N) return 0;
     int& temp=dp[x][y][now];
-    
-    if(temp==-1){
-        if(arr[x][y]==now){
-            temp=max({
-                solve(x+1, y, now), 
-                solve(x, y+1, now), 
-                solve(x+1, y, (now+1)%3)+1, 
-                solve(x, y+1, (now+1)%3)+1
-            });
-        } else temp=max(solve(x+1, y, now), solve(x, y+1, now));
-    }
+    if(temp!=-1) return temp;
+
+    if(arr[x][y]==now){
+        temp=max({
+            solve(x+1, y, now), 
+            solve(x, y+1, now), 
+            solve(x+1, y, (now+1)%3)+1, 
+            solve(x, y+1, (now+1)%3)+1
+        });
+    } else temp=max(solve(x+1, y, now), solve(x, y+1, now));
     
     return temp;
 }
