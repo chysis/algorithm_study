6f6c0136b1c3ea1773cc9f5268c0a2db39c75b28
Fe <64690761+chysis@users.noreply.github.com>
2025-06-12 21:26:43 +0900

    [Silver II] Title: 간식 파티, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/20162. 간식 파티/간식 파티.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, arr[1001], dp[1001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin>>N;
+    for(int i=0; i<N; i++) cin>>arr[i];
+    
+    for(int i=0; i<N; i++){
+        dp[i]=arr[i];
+        for(int j=0; j<i; j++){
+            if(arr[j]<arr[i]){
+                dp[i]=max(dp[i], dp[j]+arr[i]);
+            }
+        }
+    }
+    
+    cout<<*max_element(dp, dp+N);
+}
