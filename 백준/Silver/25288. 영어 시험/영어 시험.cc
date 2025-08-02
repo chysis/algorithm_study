fa1695ff5251b0e56017d930bbb28a52f13a19f3
Fe <64690761+chysis@users.noreply.github.com>
2025-08-02 20:12:28 +0900

    [Silver IV] Title: 영어 시험, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/25288. 영어 시험/영어 시험.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+string S;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin>>N>>S;
+    for(int i=0; i<N; i++){
+        cout<<S;
+    }
+}
