11c4ba392f482ca4f1b414ec1749dffb3e6756a5
Fe <64690761+chysis@users.noreply.github.com>
2025-09-13 01:57:26 +0900

    [Bronze III] Title: 거북이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2959. 거북이/거북이.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int arr[5];
+    for(int i=0; i<4; i++) cin>>arr[i];
+    sort(arr, arr+4);
+    cout<<arr[0]*arr[2];
+}
