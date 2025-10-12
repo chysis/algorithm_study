c7c63eeadb05f99b188e30c62c3e94442d26a792
Fe <64690761+chysis@users.noreply.github.com>
2025-10-12 14:04:03 +0900

    [Bronze II] Title: 비밀번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/33702. 비밀번호/비밀번호.cc
+++ 백준/Bronze/33702. 비밀번호/비밀번호.cc
@@ -1,9 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-int main()
-{
+int main(){
     int K; cin>>K;
-    if(K%2==0) cout<<0;
-    else cout<<8;
+    cout<<(K%2?8:0);
 }
