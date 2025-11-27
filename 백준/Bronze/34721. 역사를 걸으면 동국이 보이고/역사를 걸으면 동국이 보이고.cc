6ec799c48b8b9232f237c19a73be482817c89942
Fe <64690761+chysis@users.noreply.github.com>
2025-11-27 23:02:09 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N; cin>>N;
+    while(N--) cout<<"I love DGU\n";
+}
