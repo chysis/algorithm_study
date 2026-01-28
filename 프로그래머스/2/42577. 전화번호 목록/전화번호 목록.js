4a9161c29c4a16f9852ff9601387d05a2fb33165
Fe <64690761+chysis@users.noreply.github.com>
2026-01-29 01:41:51 +0900

    [level 2] Title: 전화번호 목록, Time: 155.03 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,10 @@
+// 2중 for문으로 완전 탐색 -> O(N^2) 시간 초과
+// 문자열 정렬 후(사전순으로 정렬됨) N번째가 N+1번째에 포함되는지만 확인
+function solution(phone_book) {
+    phone_book = phone_book.sort();
+    for(let i=1; i<phone_book.length; i++){
+        if(phone_book[i].startsWith(phone_book[i-1])) return false;
+    }
+    
+    return true;
+}
